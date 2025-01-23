47fd814651a4aa8da477543247db873ca9cc86c9
Sam Gerstein <sam.gerstein@osmo.ai>
2025-01-23 11:11:14 -0500

    links but no exports

--- skbuild/build.sh
+++ skbuild/build.sh
@@ -4,6 +4,6 @@ set -e
 # clean up generated dirs
 rm -rf _skbuild src/cppmordred.egg-info dist
 
-python setup.py build
-python setup.py sdist bdist_wheel
+conda run -n cpp_mordred python setup.py build
+conda run -n cpp_mordred python setup.py sdist bdist_wheel
 echo Wheel build complete
