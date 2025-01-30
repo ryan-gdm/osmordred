7bb39fff8882293d6137a14807f5b37dcafed1e3
guillaume <guillaume@davis.tail581bd.ts.net>
2025-01-30 10:03:56 -0500

    more tests

---
+++ skbuild/test/tBEState.py
@@ -0,0 +1,17 @@
+from rdkit import Chem
+import numpy as np
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor, as_completed, TimeoutError
+
+from tqdm import tqdm
+import cppmordred as rd
+
+
+
+if __name__ == "__main__":
+    smiles = ['CCCO','CCCN','c1ccccc1']
+    for s in smiles:
+        mol = Chem.MolFromSmiles(s)
+        results =  rd.CalcBEState(mol)
+        print(list(results))
+    
