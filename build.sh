2d979df66aa87ceef552859a44995b70812bc276
ryan-gdm <ryan@deepmirror.ai>
2025-08-14 11:34:59 +0100

    feat: build test python 3.13

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-export RDKIT_VERSION=2023.9.3
-export PYTHON_VERSION=3.11
-export BOOST_VERSION=1.82.0
+export RDKIT_VERSION=2025.03.1
+export PYTHON_VERSION=3.13
+export BOOST_VERSION="1.86.*"
 
 echo "Removing existing environment (if present)"
 conda env remove -y -n osmordred || true
@@ -11,13 +11,14 @@ conda env remove -y -n osmordred || true
 echo "Creating new conda environment"
 conda create -y -n osmordred \
     python=${PYTHON_VERSION} \
-    boost=${BOOST_VERSION} \
+    libboost-python=${BOOST_VERSION} \
+    libboost-devel=${BOOST_VERSION} \
+    libboost=${BOOST_VERSION} \
     eigen \
     lapack \
     ninja \
-    mordred \
     python-build \
-    rdkit=${RDKIT_VERSION} \
+    rdkit-dev=${RDKIT_VERSION} \
     blas=*=*mkl \
     -c conda-forge
 
@@ -37,6 +38,10 @@ conda run -n osmordred python -m build
 echo "✅ Wheel build complete"
 
 echo "Installing the wheel"
-pip install dist/pypi_osmordred-0.2.0-cp311-cp311-linux_x86_64.whl --force-reinstall
+pip install dist/osmordred-0.3.0-cp313-cp313-linux_x86_64.whl --force-reinstall
 
 echo "✅ Wheel installation complete"
+echo "Running post-installation tests..."
+
+cd test
+python test.py && echo "✅ Tests passed" || { echo "❌ Tests failed"; exit 1; }
